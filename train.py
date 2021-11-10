870eb9f646339e6037f4e6fb5c79fa5211732d2f
Machogu <machoguabednego6@gmail.com>
2021-11-10 19:40:46 +0300

    Fully functional implementation of the training model

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import NeuralNet
 with open('intents.json', 'r') as f:
     intents=json.load(f)
 
@@ -57,7 +58,37 @@ class ChatDataset(Dataset):
 
 #hyperparameters
 batch_size=8
+hidden_size=8
+input_size=len(x_train[0])
+output_size=len(tags)
+learning_rate=0.001
+num_epochs=1000
 
 
 dataset=ChatDataset()
-train_loader=DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader=DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size)
+
+criterion=nn.CrossEntropyLoss()
+optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words=words.to(device)
+        labels=labels.to(device)
+
+        #forward
+        outputs=model(words)
+        loss=criterion(outputs, labels)
+
+        #backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch+1) % 100==0:
+        print(f'epoch{epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
